120d9a3ba1ab75c8fae8a9b705fae27925136a0a
Jose Nicolas Santander <josess.nicolas@hotmail.com>
2020-09-14 17:38:47 -0500

    pom modificado

--- src/test/java/TestTarifaCamion.java
+++ src/test/java/TestTarifaCamion.java
@@ -11,6 +11,8 @@
 import java.text.ParseException;
 import org.junit.Test;
 import com.unicauca.tallerparqueadero.domain.*;
+//import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.*;
 public class TestTarifaCamion {
     Parqueadero parqueadero = new Parqueadero();
